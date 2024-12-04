8f7a978bcfc9b58ef77b62d7a74dec20e3c1b76b
Emmanuel Guefif <eguefif@MacBook-Pro-de-De.local>
2024-12-04 01:05:14 -0500

    rust done p2

--- rust/day4/prb.rs
+++ rust/day4/prb.rs
@@ -14,7 +14,7 @@ fn part2(input: Vec<Vec<char>>) -> usize {
     for y in 0..height {
         for x in 0..width {
             let c = input[y][x];
-            if c == 'S' || c == 'M' {
+            if c == 'A' {
                 acc += check2(&input, y, x)
             }
         }
@@ -25,24 +25,28 @@ fn part2(input: Vec<Vec<char>>) -> usize {
 fn check2(input: &Vec<Vec<char>>, y: usize, x: usize) -> usize {
     let height = input.len();
     let width = input[0].len();
-    let mut acc = 0;
 
-    if x + 3 < width && y + 3 < height {
-        let mut tmp1 = String::new();
-        tmp1.push(input[y][x]);
-        tmp1.push(input[y + 1][x + 1]);
-        tmp1.push(input[y + 2][x + 2]);
+    if x == 0 || x == width - 1 || y == 0 || y == height - 1 {
+        return 0;
+    }
+    let a1 = input[y - 1][x - 1];
+    let a2 = input[y - 1][x + 1];
+    let a3 = input[y + 1][x - 1];
+    let a4 = input[y + 1][x + 1];
 
-        let mut tmp2 = String::new();
-        tmp2.push(input[y + 2][x]);
-        tmp2.push(input[y + 1][x + 1]);
-        tmp2.push(input[y][x + 2]);
-        if check_word(&tmp1, &tmp2) {
-            acc += 1;
-        }
+    let mut w1 = String::new();
+    w1.push(a1);
+    w1.push('A');
+    w1.push(a4);
+    let mut w2 = String::new();
+    w2.push(a3);
+    w2.push('A');
+    w2.push(a2);
+    if check_word(&w1, &w2) {
+        return 1;
     }
 
-    acc
+    0
 }
 
 fn check_word(w1: &str, w2: &str) -> bool {
