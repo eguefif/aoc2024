785da1b134c144d9411efda107140e55151e248e
Emmanuel Guefif <eguefif@MacBook-Pro-de-De.local>
2024-12-05 20:24:54 -0500

    Done in rust

--- rust/day5/prb.rs
+++ rust/day5/prb.rs
@@ -1,54 +1,110 @@
-use std::collections::HashMap;
-
 fn main() {
-    //let (input, rules) = get_input();
     let (rules, input) = get_input();
 
-    println!("{:?}\n{:?}", rules, input);
-    //println!("Part1: {}", part1(input.clone(), rules.clone()));
-    //println!("Part2: {}", part2(input.clone(), rules.clone()));
+    println!("Part1: {}", part1(input.clone(), rules.clone()));
+    println!("Part2: {}", part2(input.clone(), rules.clone()));
 }
 
-fn part1(input: Vec<Vec<u32>>, rules: HashMap<u32, u32>) -> u32 {
-    0
+fn part1(input: Vec<Vec<u32>>, rules: Vec<(u32, u32)>) -> u32 {
+    input.iter().fold(0, |mut acc, row| {
+        if is_sorted(row, &rules) {
+            let n = row.len() / 2 as usize;
+            acc += row[n];
+        }
+        acc
+    })
 }
 
-fn get_input() -> (HashMap<u32, u32>, Vec<Vec<u32>>) {
-    let content = include_str!("../../inputs/exemple");
-    let chunks: Vec<&str> = content.split("\n\n").collect::<Vec<&str>>();
+fn is_sorted(row: &Vec<u32>, rules: &Vec<(u32, u32)>) -> bool {
+    row.into_iter()
+        .zip(row[1..row.len()].iter())
+        .all(|(a, b)| check(*a, *b, &rules))
+}
 
-    let mut rules = HashMap::new();
-    let chunk_rules = chunks[0].split('\n').collect::<Vec<&str>>();
-    for chunk in chunk_rules {
-        let r: Vec<&str> = chunk.split("|").collect::<Vec<&str>>();
-        rules.insert(r[0].parse::<u32>().unwrap(), r[1].parse::<u32>().unwrap());
+fn check(a: u32, b: u32, rules: &Vec<(u32, u32)>) -> bool {
+    for rule in rules.iter() {
+        if rule.0 == b && rule.1 == a {
+            return false;
+        }
+    }
+    return true;
+}
+
+fn part2(input: Vec<Vec<u32>>, rules: Vec<(u32, u32)>) -> u32 {
+    let input_p2 = get_unsorted(input, &rules);
+    input_p2.iter().fold(0, |acc, row| {
+        let n = row.len().div_euclid(2);
+        let sorted = sort_row(&mut row.clone(), &rules);
+        acc + sorted[n]
+    })
+}
+
+fn sort_row(row: &mut Vec<u32>, rules: &Vec<(u32, u32)>) -> Vec<u32> {
+    for i in 0..row.len() {
+        for j in (i + 1)..row.len() {
+            if !check(row[i], row[j], &rules) {
+                let tmp = row[i];
+                row[i] = row[j];
+                row[j] = tmp;
+            }
+        }
+    }
+    row.clone()
+}
+
+fn get_unsorted(input: Vec<Vec<u32>>, rules: &Vec<(u32, u32)>) -> Vec<Vec<u32>> {
+    let mut retval: Vec<Vec<u32>> = vec![];
+    input.iter().for_each(|row| {
+        if !is_sorted(row, &rules) {
+            retval.push(row.clone());
+        }
+    });
+    retval
+}
+
+fn get_input() -> (Vec<(u32, u32)>, Vec<Vec<u32>>) {
+    let content = include_str!("../../inputs/d5");
+    let mut chunks = content.split("\n\n");
+
+    let mut rules = Vec::new();
+    for chunk in chunks.next().unwrap().lines() {
+        let mut parts = chunk.split("|");
+        rules.push((
+            parts.next().unwrap().parse::<u32>().unwrap(),
+            parts.next().unwrap().parse::<u32>().unwrap(),
+        ));
     }
     let mut input: Vec<Vec<u32>> = vec![];
 
-    let chunk_input = chunks[1].split('\n').collect::<Vec<&str>>();
-    for chunk in chunk_input {
-        let row = chunk
-            .split(',')
-            .filter(|value| !value.is_empty())
-            .map(|value| value.parse::<u32>().unwrap())
-            .collect();
-        input.push(row);
+    for chunk in chunks.next().unwrap().lines() {
+        input.push(
+            chunk
+                .split(',')
+                .filter(|value| !value.is_empty())
+                .map(|value| value.parse::<u32>().unwrap())
+                .collect(),
+        );
     }
-    (
-        rules,
-        input
-            .into_iter()
-            .filter(|value| !value.is_empty())
-            .collect(),
-    )
+    (rules, input)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    #[test]
     fn test_part1() {
-        let (input, part1) = get_input();
-        let res = part1(input, part1);
+        let (input, rules) = get_input();
+        let res = part1(rules, input);
+
+        assert_eq!(6505, res)
+    }
+
+    #[test]
+    fn test_part2() {
+        let (input, rules) = get_input();
+        let res = part2(rules, input);
+
+        assert_eq!(6897, res)
     }
 }
