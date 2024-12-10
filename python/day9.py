8d734a43040eccc2eaac1f73b4daf605b169248d
Emmanuel Guefif <eguefif@MacBook-Pro-de-De.local>
2024-12-09 20:12:40 -0500

    Not quite the right answer

--- python/day9.py
+++ python/day9.py
@@ -95,15 +95,90 @@ def dump(disk, index, disk_c, index_c):
     print()
 
 
-def remap2(disk, index):
-    if is_disk_finishing_with_a_file(index):
-        disk_c = Cursor(fr=len(disk) - 1, to=index[0])
-        index_c = Cursor(fr=len(index) - 1, to=1)
-    else:
-        disk_c = Cursor(fr=len(disk) - 1 - index[-1], to=index[0])
-        index_c = Cursor(fr=len(index) - 1, to=1)
+def init_cursor(disk, index):
+    disk_c = Cursor(0, 0)
 
-    while index_c.to < len(index) and disk_c.to < len(disk):
+    i = 0
+    while i < len(disk):
+        if disk[i] != ".":
+            i += 1
+            continue
+        disk_c.to = i
+        break
+
+    i = len(disk) - 1
+    while i >= 0:
+        if disk[i] == ".":
+            i -= 1
+            continue
+        disk_c.fr = i
+        break
+
+    index_c = Cursor(0, len(index) - 1)
+
+    i = len(index) - 1
+    if disk[len(disk) - 1] != ".":
+        index_c.fr = i
+    else:
+        while i > 0:
+            if i % 2 != 0:
+                i -= 1
+                continue
+            if index[i] != 0:
+                index_c.fr = i
+                break
+
+            i -= 1
+
+    for i, v in enumerate(index):
+        if i % 2 == 0:
+            i += 1
+            continue
+        if v != 0:
+            index_c.to = i
+            break
+        i += 1
+
+        if disk_c.to > disk_c.fr:
+            print("t", disk_c.to)
+            print(disk_c.fr)
+            disk_c.to = -1
+    return disk_c, index_c
+
+
+def init(disk):
+    index = []
+    counter = 0
+    for i, c in enumerate(disk):
+        if (
+            i + 1 < len(disk)
+            and c != disk[i + 1]
+            and type(c) is type(disk[i + 1])
+            and i != 0
+        ):
+            index.append(counter)
+            index.append(0)
+            counter = 0
+        elif i + 1 < len(disk) and c is not disk[i + 1]:
+            index.append(counter)
+            counter = 0
+        elif i + 1 >= len(disk):
+            index.append(counter)
+            break
+        counter += 1
+
+    disk_c, index_c = init_cursor(disk, index)
+
+    return index, disk_c, index_c
+
+
+def remap2(disk):
+    print(len(disk))
+    index, disk_c, index_c = init(disk)
+    print(len(index))
+    counter = 0
+
+    while disk_c.to != -1 and counter < 10:
         while index_c.fr > index_c.to and disk_c.fr > 0:
             dump(disk, index, disk_c, index_c)
             if index[index_c.to] >= index[index_c.fr]:
@@ -121,22 +196,16 @@ def remap2(disk, index):
                 n = index[index_c.fr] + index[index_c.fr - 1]
                 index_c.fr -= 2
                 disk_c.fr -= n
-        if is_disk_finishing_with_a_file(index):
-            disk_c.fr = len(disk) - 1
-            index_c.fr = len(disk) - 1
-        else:
-            disk_c.fr = len(disk) - 1 - index[-1]
-            index_c.fr = len(disk) - 1 - index[-1]
-        n = index[index_c.to] + index[index_c.to + 1]
-        index_c.to += 2
-        disk_c.to += n
+            counter += 1
+
+        index, disk_c, index_c = init(disk)
 
     return disk
 
 
 def part2(line):
     disk = put_id(line)
-    disk = remap2(disk, line)
+    disk = remap2(disk)
     return calculate(disk)
 
 
