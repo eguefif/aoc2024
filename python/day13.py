9626c8e94a4e49bc3e220848f454793a20a57dea
Emmanuel Guefif <eguefif@MacBook-Pro-de-De.local>
2024-12-15 11:05:03 -0500

    Base solution

--- python/day13.py
+++ python/day13.py
@@ -25,16 +25,19 @@ class Machine:
         return (int(x), int(y))
 
     def get_mina(self):
-        max1 = self.prize[0] / self.a[0]
-        max2 = self.prize[1] / self.a[1]
-        return max(int(max1), int(max2))
+        min1 = self.prize[0] / self.a[0]
+        min2 = self.prize[1] / self.a[1]
+        return min(int(min1) + 1, int(min2) + 1)
+
+    def get_minb(self):
+        min1 = self.prize[0] / self.b[0]
+        min2 = self.prize[1] / self.b[1]
+        return min(int(min1) + 1, int(min2) + 1)
 
     def __str__(self):
         retval = f"A({self.a[0]}, {self.a[1]}), B({self.b[0], self.b[1]})"
         retval += f" Prize: ({self.prize[0], self.prize[1]})"
         return retval
-
-    # with open("../inputs/exemple", "r") as f:
 
 
 with open("../inputs/d13", "r") as f:
@@ -43,27 +46,14 @@ with open("../inputs/d13", "r") as f:
     machines = [Machine(machine.strip()) for machine in machines]
 
 
-def get_cost(machine):
-    mina = machine.get_mina()
-    prices = []
-
-    for a in range(0, mina):
-        new_x = machine.prize[0] - machine.a[0] * a
-        new_y = machine.prize[1] - machine.a[1] * a
-        if new_x % machine.b[0] == 0 and new_y % machine.b[1] == 0:
-            b = int(new_x / machine.b[0])
-            prices.append(a * 3 + b)
-
-    if len(prices) == 0:
-        return 0
-    return min(prices)
+def dump(machines):
+    [print(machine) for machine in machines]
 
 
 def part1(machines):
     acc = 0
     for machine in machines:
-        retval = get_cost(machine)
-        acc += retval
+        acc += get_cost(machine)
     return acc
 
 
@@ -71,5 +61,6 @@ ans1 = part1(machines)
 
 print(ans1)
 
-assert ans1 == 480
+# assert ans1 == 480
+assert ans1 == 26299
 # 5466
