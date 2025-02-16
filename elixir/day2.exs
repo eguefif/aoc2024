2e9832184e48625c62e2f80f77ecec65c1f62d3e
Emmanuel Guefif <eguefif@fastmail.com>
2025-02-16 14:45:45 -0500

    Elixir day2 p2

--- elixir/day2.exs
+++ elixir/day2.exs
@@ -22,13 +22,24 @@ defmodule Day2 do
     ((b - a) * sign in 1..3) && check_gap?([b | rest], sign)
   end
 
-  def part2() do
+  def part2(input) do
+    input
+      |>Enum.count(fn x -> check_damp(x, []) end)
+  end
+
+  defp check_damp([], prefix) do
+    safe?(prefix)
+  end
+
+  defp check_damp([head | tail], prefix) do
+    safe?(Enum.reverse(tail, prefix)) or check_damp(tail, [head | prefix])
   end
 end
 
-data = "../input/example-day2.txt"
+data = "../input/day2.txt"
     |> File.read!()
     |> String.split("\n", trim: true)
     |> Day2.parse()
 
 IO.puts "Part1: #{Day2.part1(data)}"
+IO.puts "Part2: #{Day2.part2(data)}"
