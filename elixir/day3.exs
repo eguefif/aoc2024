1a36ac9df80345d0c43db3ffea5e6ed9ea6ecb18
Emmanuel Guefif <eguefif@fastmail.com>
2025-02-16 20:17:12 -0500

    Elixir day3 p2

--- elixir/day3.exs
+++ elixir/day3.exs
@@ -8,6 +8,25 @@ defmodule Day3 do
       |> Enum.map(fn [_, x, y] -> {String.to_integer(x), String.to_integer(y)} end)
       |> Enum.reduce(0, fn {x, y}, acc -> acc + x * y end)
   end
+
+  def part2(input) do
+    Regex.scan(~r/(?>mul\((\d+),(\d+)\))|(?>do\(\))||(?>don't\(\))/, input)
+      |>Enum.filter(&(&1 != [""]))
+      |>Enum.reduce({true, 0}, fn token, {continue?, total} ->
+        case {continue?, token} do
+          {_, ["do()"]} -> {true, total}
+          {_, ["don't()"]} -> {false, total}
+          {true, token} -> {true, total + multiply(token)}
+          {false, _} -> {false, total}
+        end
+      end)
+      |>elem(1)
+  end
+
+  def multiply([_ | op]) do
+    String.to_integer(Enum.at(op, 0)) * String.to_integer(Enum.at(op, 1))
+
+  end
 end
 
 data = "../input/day3.txt"
@@ -15,3 +34,4 @@ data = "../input/day3.txt"
   |> Day3.parse()
 
 IO.puts "Part1: #{Day3.part1(data)}"
+IO.puts "Part2: #{Day3.part2(data)}"
