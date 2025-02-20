018703faf3623f25e054e127b58bd9e2a8daa2c8
Emmanuel Guefif <eguefif@fastmail.com>
2025-02-20 17:38:32 -0500

    Elixir day 5 p2

--- elixir/day5.exs
+++ elixir/day5.exs
@@ -33,10 +33,25 @@ defmodule Day5 do
     end)
     |>Enum.all?()
   end
+
+  def part2({orders, updates}) do
+    updates
+    |>Enum.filter(&(not ordered?(&1, orders)))
+    |>Enum.map(&(Enum.sort(&1, fn a, b -> comp({a, b}, orders) end)))
+    |>Enum.map(fn update ->
+      mid = round(length(update) / 2) - 1
+      Enum.at(update, mid)
+    end)
+    |>Enum.sum()
+  end
+
+  def comp({a, b}, orders) do
+    not ({b, a} in orders)
+  end
 end
 
-input = {"../input/day5.txt", 6505}
-#input = {"../input/day5-example.txt", 143}
+input = {"../input/day5.txt", 6505, 6897}
+#input = {"../input/day5-example.txt", 143, 123}
 
 data = elem(input, 0)
   |>File.read!()
@@ -44,4 +59,7 @@ data = elem(input, 0)
 
 p1 = Day5.part1(data)
 IO.puts "Part1: #{p1} correct? #{p1 == elem(input, 1)}"
+
+p2 = Day5.part2(data)
+IO.puts "Part2: #{p2} correct? #{p2 == elem(input, 2)}"
 
