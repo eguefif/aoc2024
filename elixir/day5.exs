72344d322ae30dd503ef1b07393d4bae9d67b45b
Emmanuel Guefif <eguefif@fastmail.com>
2025-02-20 17:28:19 -0500

    Elixir day 5 p1

---
+++ elixir/day5.exs
@@ -0,0 +1,47 @@
+defmodule Day5 do
+  def parse(content) do
+    [orders_data, updates_data] = content
+    |>String.split("\n\n", trim: true)
+    orders = orders_data
+      |>String.split("\n", trim: true)
+      |>Enum.map(&(String.split(&1, "|")))
+      |>Enum.map(&(List.to_tuple(&1)))
+      |>Enum.map(fn {n1, n2} -> {String.to_integer(n1), String.to_integer(n2)} end)
+    updates = updates_data
+      |>String.split("\n", trim: true)
+      |>Enum.map(fn line ->
+        String.split(line, ",")
+          |>Enum.map(fn n -> String.to_integer(n) end) end)
+    {orders, updates}
+  end
+
+  def part1({orders, updates}) do
+    updates
+    |>Enum.filter(&(ordered?(&1, orders)))
+    |>Enum.map(fn update ->
+      mid = round(length(update) / 2) - 1
+      Enum.at(update, mid)
+    end)
+    |>Enum.sum()
+  end
+
+  def ordered?(update, orders) do
+    update
+    |>Enum.chunk_every(2, 1)
+    |>Enum.map(fn chunk ->
+      length(chunk) == 1 || not ({Enum.at(chunk, 1), Enum.at(chunk, 0)} in orders)
+    end)
+    |>Enum.all?()
+  end
+end
+
+input = {"../input/day5.txt", 6505}
+#input = {"../input/day5-example.txt", 143}
+
+data = elem(input, 0)
+  |>File.read!()
+  |>Day5.parse()
+
+p1 = Day5.part1(data)
+IO.puts "Part1: #{p1} correct? #{p1 == elem(input, 1)}"
+
